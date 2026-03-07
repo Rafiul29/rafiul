e711b6261887dffd7f4ef02deaecbf54734828cf
Rafiul29 <rafiulislam4472@gmail.com>
2026-03-07 12:00:24 +0600

    bug fixing

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,6 +12,8 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_APP_URL,
     emailAndPassword: {
         enabled: true,
     },
