84a95cb747b872c3b558329e8b202943ec1a1a45
Rafiul29 <rafiulislam4472@gmail.com>
2026-03-11 02:24:03 +0600

    bug fixing

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,13 +1,12 @@
 import About from "@/components/About";
-import { Blogs } from "@/components/Blogs";
-import { Certifications } from "@/components/Certifications";
 import { Contact } from "@/components/Contact";
 import { Education } from "@/components/Education";
 import { Experience } from "@/components/Experience";
-import { GitHub } from "@/components/GitHub";
 import Hero from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import { Skills } from "@/components/Skills";
+import { ClientComponentWrapper } from "@/components/ClientComponentWrapper";
+
 export default function Home() {
   return (
     <main>
@@ -17,9 +16,9 @@ export default function Home() {
       <Experience />
       <Projects />
       <Education />
-      <GitHub />
-      <Certifications />
-      {/* <Blogs /> */}
+      <ClientComponentWrapper component="GitHub" />
+      <ClientComponentWrapper component="Certifications" />
+      {/* <ClientComponentWrapper component="Blogs" /> */}
       <Contact />
     </main>
   );
